cbb154fffafa9c42deaeba0dad6108072aa79f3c
lc0709 <3dollee@gmail.com>
2024-09-18 01:48:27 +0900

    Feat: Response

--- src/main/kotlin/zinc/doiche/core/service/openai/OpenAIService.kt
+++ src/main/kotlin/zinc/doiche/core/service/openai/OpenAIService.kt
@@ -1,5 +1,8 @@
 package zinc.doiche.core.service.openai
 
+import zinc.doiche.core.domain.BuntaMessage
+
 interface OpenAIService {
 
+    suspend fun requestMessageContext(messageList: List<BuntaMessage>): OpenAIResponse
 }
